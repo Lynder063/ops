b927b772b71178b3d052a918841b9e30fbb12d65
Lynder063 <lynderjekokot@gmail.com>
2024-02-21 17:09:31 +0100

    Doc. for cv3_2.sh

--- cv3/cv3_2.sh
+++ cv3/cv3_2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# $# je proměná která obsahuje počet argumentů předaných do scriptu
-# Dáme že pokud byl zadaný jiný počet než 1, vypíše echo a dát exit 1
 
 if [ $# -ne 1 ]; then
     echo "Chyba: Zadejte název souboru jako argument!"
@@ -12,9 +10,14 @@ if [ ! -f "$1" ]; then
     echo "Chyba: Soubor $1 neexistuje!"
     exit 1
 fi
+# Dokumentaci k tomuto kodu najdeme v 'cv3_1.sh'
 
-# 
 
 line=$(sed -n '5p' "$1")
+# Tento řádek uloží do proměné 'line'
+# Výsledek příkazu $sed -n '5p' "$1"
+# 5p je pátý řádel
+# $1 je definováná proměná která je náš soubor
+
 echo "5. řádek souboru $1: $line"
 
