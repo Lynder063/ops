17dfe20d1d8799cae3a571befa58e0fb50f56f16
Lynder063 <lynderjekokot@gmail.com>
2024-02-27 21:42:17 +0100

    fix

--- cv3/cv3_4.sh
+++ cv3/cv3_4.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Vytvoření nového uživatele, uživatel se dá vytvořit jen pomocí sudo
-# "krystofjebuh"
-
-sudo adduser krystofjebuh
 
 # Čekání na přihlášení nového uživatele
 until who | grep -q krystofjebuh; do
