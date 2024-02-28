2ddf74c0e1890eeb1a4757163ee971b861b08748
Lynder063 <lynderjekokot@gmail.com>
2024-02-28 14:00:24 +0100

    Fixed with getting text from argument

--- cv3/cv3_5.sh
+++ cv3/cv3_5.sh
@@ -2,7 +2,7 @@
 
 # Výpis informací o argumentech
 echo "Jméno a příjmení: $1"
-echo "Počet argumentů: $#"
+echo "Počet argumentů: $2"
 echo "Název skriptu: $0"
 echo "Domovský adresář: $HOME"
 echo "ID procesu skriptu: $$"
