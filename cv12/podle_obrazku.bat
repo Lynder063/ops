97469b1908fdc720cf390408d2734eba5581b432
Lynder063 <lynderjekokot@gmail.com>
2024-02-28 18:46:01 +0100

    fixes

---
+++ cv12/podle_obrazku.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem Zobrazit zprávu
+echo Ahoj, vitam te zpatky!
+
+rem Získat datum a čas
+rem (Použijte "rem" před řádkem, který nechcete spustit)
+rem set "datum=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%"
+rem set "cas=%TIME:~0,2%%TIME:~3,2%"
+
+rem Zobrazit datum a čas
+rem (Použijte "rem" před řádkem, který nechcete spustit)
+rem echo Dnes je: %datum% %cas%
+
+rem Čekat na stisk klávesy
+pause
