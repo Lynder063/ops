1235abf9e0ef12fde58ac09374794a426db87d70
Lynder063 <lynderjekokot@gmail.com>
2024-02-28 15:34:23 +0100

    cv3_5.sh fix

--- cv3/cv3_5.sh
+++ cv3/cv3_5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Výpis informací o argumentech
-echo "Jméno a příjmení: $1"
-echo "Počet argumentů: $2"
+echo "Jméno a příjmení: $1 + $2"
+echo "Počet argumentů: $#"
 echo "Název skriptu: $0"
 echo "Domovský adresář: $HOME"
 echo "ID procesu skriptu: $$"
